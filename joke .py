ec17dabefe41729de3b65980fc35c18995a2b552
sarthak gaikwad <sarthakrg14@gmail.com>
2022-11-19 15:05:27 +0530

    first commit

--- joke .py
+++ joke .py
@@ -1,11 +1,11 @@
-# from athena import speak
+
 import pyjokes
 import speech_recognition as sr
 import pyttsx3
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-# print(voices[1].id)
+
 engine.setProperty('voice',voices[1].id)
 
 def speak(audio):
